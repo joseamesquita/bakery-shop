1ad4837487a237c62ffe856ee539b6b3a8b2a704
Student <example@example.com>
2020-02-28 08:28:13 -0800

    add two classes, Bakery and Pastry

---
+++ Models/Bakery.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Model
+{
+    public class Bakery
+    {
+        public Bakery()
+        {
+
+        }
+    }
+    public  class Pastry
+    {
+        public Pastry()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Model;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
