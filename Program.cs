40365aa54695766d0d633c65f1817753df0c4d6b
Student <example@example.com>
2020-02-28 08:42:32 -0800

    add prompt on the console

--- Program.cs
+++ Program.cs
@@ -7,7 +7,15 @@ namespace Program
     {
         public static void Main()
         {
-            
+            Program.Greeting();
+        }
+
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello, would you like to purchase any bread or pastries? [YES] or [NO]");
+            string response = Console.ReadLine();
+            Console.WriteLine(response.ToUpper());
+
         }
     }
 }
