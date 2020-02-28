69fb75b7d13172f06b20891df625f4d86bea8160
Student <example@example.com>
2020-02-28 10:34:42 -0800

    add order method for user response to the prompt

--- Program.cs
+++ Program.cs
@@ -18,14 +18,24 @@ namespace Program
 
             if( response == "YES")
             {
-                Console.WriteLine("Today's specials include, but 2 get 1 free for bread or a single loaf costs $5.");
-                Console.WriteLine("For pastries, but 1 for $2 or 3 for $5.");
+                Program.Order();
+                
             }
             else
             {
                 Console.WriteLine("Have a great day. We look forward in seeing you soon.");
             }
 
+        }
+        public static void Order()
+        {
+            Console.WriteLine("Today's specials include, but 2 get 1 free for bread or a single loaf costs $5.");
+            Console.WriteLine("For pastries, but 1 for $2 or 3 for $5.");
+
+            int order = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(order);
+            Console.WriteLine("----------------------");
         }
     }
 }
