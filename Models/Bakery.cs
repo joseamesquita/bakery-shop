04ec1ad84ea739588edf26997e2d5ce7af73aab9
Student <example@example.com>
2020-02-28 09:04:55 -0800

    add prompts for bread and pastry specials

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -3,17 +3,25 @@ using System;
 namespace Model
 {
     public class Bakery
-    {
-        public Bakery()
-        {
+    {   
+        public int Loaf {get; set;}
+        public int Cost {get; set;}
 
+        public Bakery(int loaf)
+        {
+            Loaf = loaf;
+        }
+        public void BreadCost()
+        {
+            if (Loaf == 1)
+            {   
+                int Cost = 5;
+                Console.WriteLine("Your total cost for one loaf oof bread is: " + Cost);
+            }
         }
     }
     public  class Pastry
     {
-        public Pastry()
-        {
-
-        }
+    
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,8 +13,18 @@ namespace Program
         public static void Greeting()
         {
             Console.WriteLine("Hello, would you like to purchase any bread or pastries? [YES] or [NO]");
-            string response = Console.ReadLine();
-            Console.WriteLine(response.ToUpper());
+            string response = Console.ReadLine().ToUpper();
+            Console.WriteLine("--------------------");
+
+            if( response == "YES")
+            {
+                Console.WriteLine("Today's specials include, but 2 get 1 free for bread or a single loaf costs $5.");
+                Console.WriteLine("For pastries, but 1 for $2 or 3 for $5.");
+            }
+            else
+            {
+                Console.WriteLine("Have a great day. We look forward in seeing you soon");
+            }
 
         }
     }
