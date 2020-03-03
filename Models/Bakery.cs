3f7e64704e07bf61ba6e599f2eb67c5325a05c5e
Jose Amesquita <joseamesquita11@gmail.com>
2020-03-02 20:48:25 -0800

    correct spacing

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -2,84 +2,84 @@ using System;
 
 namespace Model
 {
-    public class Bakery
-    {   
-        public int Loaf {get; set;}
-        public int Cost {get; set;}
+  public class Bakery
+  {
+    public int Loaf { get; set; }
+    public int Cost { get; set; }
 
-        public Bakery(int loaf)
-        {
-            Loaf = loaf;
-            Cost = 5;
-        }
-        public void BreadCost()
-        {
-            if (Loaf == 1)
-            {   
-                Console.WriteLine("Your total cost for 1 loaf of bread is: " + "$" + Cost);
-            }
-            else if (Loaf == 2 || Loaf == 3 || Loaf == 4)
-            {
-                Cost = 10; 
-                Console.WriteLine("total cost for " + Loaf + " loafs of bread: " + "$" + Cost + "\n");
-            }
-            else if (Loaf == 5 || Loaf == 6)
-            {
-                Cost = 20;
-                Console.WriteLine("total cost for " + Loaf + " loafs of bread: " + "$" + Cost + "\n");
-            }
-            else 
-            {
-                Console.WriteLine("Sorry, we are only able to sell a quantity amount between 1-6 loafs of bread per guest. \n");
-            }
-        }
-    }
-    public class Pastries
+    public Bakery(int loaf)
     {
-        public int Pastry {get; set;}
-        public int Cost {get; set;}
-
-        public Pastries(int pastry)
-        {
-            Pastry = pastry;
-            Cost = 0;
-        }
-        public void PastryCost()
-        {
-            if (Pastry == 1)
-            {   
-                Cost += 2;
-                Console.WriteLine("Your total cost for 1 pastry: " + "$" + Cost);
-            }
-            else if (Pastry == 2)
-            {
-                Cost += 4;
-                Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
-            }
-            else if (Pastry == 3)
-            {
-                Cost += 5;
-                Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
-            }
-            else if (Pastry == 4)
-            {
-                Cost += 7;
-                Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
-            }
-            else if (Pastry == 5)
-            {
-                Cost += 9;
-                Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
-            }
-            else if (Pastry == 6)
-            {
-                Cost += 10;
-                Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
-            }
-            else 
-            {
-                Console.WriteLine("Sorry, we are only able to sell a quantity amount between 1-6 pastries per guest. \n");
-            }
-        }
+      Loaf = loaf;
+      Cost = 5;
     }
+    public void BreadCost()
+    {
+      if (Loaf == 1)
+      {
+        Console.WriteLine("Your total cost for 1 loaf of bread is: " + "$" + Cost);
+      }
+      else if (Loaf == 2 || Loaf == 3 || Loaf == 4)
+      {
+        Cost = 10;
+        Console.WriteLine("total cost for " + Loaf + " loafs of bread: " + "$" + Cost + "\n");
+      }
+      else if (Loaf == 5 || Loaf == 6)
+      {
+        Cost = 20;
+        Console.WriteLine("total cost for " + Loaf + " loafs of bread: " + "$" + Cost + "\n");
+      }
+      else
+      {
+        Console.WriteLine("Sorry, we are only able to sell a quantity amount between 1-6 loafs of bread per guest. \n");
+      }
+    }
+  }
+  public class Pastries
+  {
+    public int Pastry { get; set; }
+    public int Cost { get; set; }
+
+    public Pastries(int pastry)
+    {
+      Pastry = pastry;
+      Cost = 0;
+    }
+    public void PastryCost()
+    {
+      if (Pastry == 1)
+      {
+        Cost += 2;
+        Console.WriteLine("Your total cost for 1 pastry: " + "$" + Cost);
+      }
+      else if (Pastry == 2)
+      {
+        Cost += 4;
+        Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
+      }
+      else if (Pastry == 3)
+      {
+        Cost += 5;
+        Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
+      }
+      else if (Pastry == 4)
+      {
+        Cost += 7;
+        Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
+      }
+      else if (Pastry == 5)
+      {
+        Cost += 9;
+        Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
+      }
+      else if (Pastry == 6)
+      {
+        Cost += 10;
+        Console.WriteLine("total cost for " + Pastry + " pastries: " + "$" + Cost + "\n");
+      }
+      else
+      {
+        Console.WriteLine("Sorry, we are only able to sell a quantity amount between 1-6 pastries per guest. \n");
+      }
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -3,62 +3,64 @@ using Model;
 
 namespace Program
 {
-    public class Program
+  public class Program
+  {
+    public static void Main()
     {
-        public static void Main()
-        {
-            Program.Greeting();
-        }
-
-        public static void Greeting()
-        {
-            Console.WriteLine("Hello, would you like to purchase any bread or pastries? [YES] or [NO] \n");
-            string response = Console.ReadLine().ToUpper();
-            
-            if( response == "YES")
-            {
-                Program.Order();
-                
-            }
-            else
-            {
-                Console.WriteLine("Have a great day. We look forward in seeing you soon.");
-            }
-
-        }
-        public static void Order()
-        {
-            Console.WriteLine("\n ----- Today's specials include ----- ");
-            Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
-            Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5.");
-            Console.WriteLine("------------------------------------- \n");
-            Console.WriteLine("Would you like to order Bread or Pastries? [BREAD] or [PASTRIES] \n");
-            string item = Console.ReadLine().ToUpper();
-            
-            if (item == "BREAD")
-            {
-                Console.WriteLine("\n" + "How many loaf(s) of bread would you like to purchase?");
-
-                int order = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("------------------------------------- \n");
-                Bakery bread = new Bakery(order);
-                bread.BreadCost();
-            }
-            
-            else if (item == "PASTRIES") {
-                Console.WriteLine("\n" + "How many pastries would you like to purchase?");
-
-                int order = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("------------------------------------- \n");
-                Pastries pastry = new Pastries(order);
-                pastry.PastryCost();
-            }
-            
-            else {
-                Console.WriteLine("Please type BREAD or PASTRIES");
-            }
-        }
+      Program.Greeting();
     }
+
+    public static void Greeting()
+    {
+      Console.WriteLine("Hello, would you like to purchase any bread or pastries? [YES] or [NO] \n");
+      string response = Console.ReadLine().ToUpper();
+
+      if (response == "YES")
+      {
+        Program.Order();
+
+      }
+      else
+      {
+        Console.WriteLine("Have a great day. We look forward in seeing you soon.");
+      }
+
+    }
+    public static void Order()
+    {
+      Console.WriteLine("\n ----- Today's specials include ----- ");
+      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5.");
+      Console.WriteLine("------------------------------------- \n");
+      Console.WriteLine("Would you like to order Bread or Pastries? [BREAD] or [PASTRIES] \n");
+      string item = Console.ReadLine().ToUpper();
+
+      if (item == "BREAD")
+      {
+        Console.WriteLine("\n" + "How many loaf(s) of bread would you like to purchase?");
+
+        int order = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("------------------------------------- \n");
+        Bakery bread = new Bakery(order);
+        bread.BreadCost();
+      }
+
+      else if (item == "PASTRIES")
+      {
+        Console.WriteLine("\n" + "How many pastries would you like to purchase?");
+
+        int order = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("------------------------------------- \n");
+        Pastries pastry = new Pastries(order);
+        pastry.PastryCost();
+      }
+
+      else
+      {
+        Console.WriteLine("Please type BREAD or PASTRIES");
+      }
+    }
+  }
 }
